6d474fed4fde7d207310865453afe3d6daa87283
Leo Vdw <l.vdw@hotmail.fr>
2018-12-11 17:09:20 +0100

    Update

---
+++ ressources/themes/900K-Theme/templates/view/campagne.php
@@ -0,0 +1,97 @@
+<div class="conceptions" >
+  <div class="conceptions--title" style="background-image : url('<?= IMAGES_URL; ?>/conceptions.png')">
+    <h1>Vous êtes victimes de vos <span>préjugés<span></h1>
+    <p>Ne culpabilisez pas, vous êtes juste victime d’un processus
+      inconscient de votre cerveau qui vous donne une vision
+      biaisé de la réalité.
+    </p>
+  </div>
+
+  <div class="conceptions__brain">
+    <div class="conceptions__brain--img" style="background-image : url('<?= IMAGES_URL; ?>/brain-and-head.png')"></div>
+    <div class="conceptions__brain--content">
+      <p>Notre cerveau a besoin de résumer les informations qui lui parviennent de façon simple pour aller bien. Ces informations sont principalement transmises par la culture et notre l’éducation. Voilà pourquoi nous ne sommes capable de voir le handicap qu'à travers le prisme de notre réalité et donc de nos stéréotypes.</p>
+    </div>
+  </div>
+
+  <div class="conceptions__vdo">
+    <div class="conceptions__vdo--content">
+      <p>Découvrez la vidéo explicative de Patrick Scharnitzk docteur et manager chargé d'accompagner les entreprises sur les stéréotypes en entreprise.</p>
+    </div>
+    <div class="conceptions__vdo--iframe">
+      <iframe src="https://www.youtube.com/embed/s5SvQgwUmBQ" width="350" height="450" frameborder="0"></iframe>
+    </div>
+  </div>
+  <div class="conceptions--content">
+    <p>Bon, vous avez pris conscience que nous avions tous des stéréotypes existants envers les personnes en situation de handicap et vous savez à présent qu’ils sont dû à un processus inconscient. </p>
+  </div>
+
+  <div class="conceptions__solutions">
+    <div class="conceptions__solutions--content">
+      <h3>Que faire alors pour y remédier ?</h3>
+      <p>On ne se débarrasse pas de ces stéréotypes. La société le fait à l’échelle d’une génération, deux, trois, quatre… On met cinquante ans pour faire bouger un stéréotype. Cinquante ans c’est trop long à l’échelle d’une carrière. Par contre, on peut essayer de faire le travail d’accepter leur existence, avec beaucoup d’humilité, avec un travail sur soi-même, d’arrêter de se taper la tête contre les murs d’une façon moralisatrice, par contre, travailler sur le passage à l’acte.</p>
+    </div>
+    <div class="conceptions__solutions--img" style="background-image : url('<?= IMAGES_URL; ?>/family.png')"></div>
+  </div>
+
+  <div class="conceptions__action">
+    <div class="ation">
+      <h3>Comment passer à l’acte à votre échelle?</h3>
+      <p>Vous pouvez dès à présent avoir un impact sur votre entreprise en réservant un atelier d’accompagnement pour sensibiliser vos employés sur le sujet du handicap.</p>
+      <a href="#">
+        <button class="mdc-button mdc-button--raised mdc-ripple-upgraded">
+          En savoir plus
+        </button>
+      </a>
+    </div>
+  </div>
+
+  <h2>
+    Vous désirez en savoir plus sur le handicap au travail ?
+    Ces articles peuvent vous intéresser
+  </h2>
+  <div class="conceptions__articles--wraper">
+    <div class="conceptions__articles">
+      <div class="conceptions__articles--content">
+        <p>Apportez une valeur ajoutée à votre
+entreprise et faites valoir votre RSE grâce à l’embauche d’employés handicapés </p>
+      </div>
+      <a href="#">
+        <button class="mdc-button mdc-button--raised mdc-ripple-upgraded">
+          En savoir plus
+        </button>
+      </a>
+    </div>
+    <div class="conceptions__articles">
+      <div class="conceptions__articles--content">
+        <p>Pôle Emploi et Cap emploi mettent en lien handicapés et entreprises </p>
+      </div>
+      <a href="#">
+        <button class="mdc-button mdc-button--raised mdc-ripple-upgraded">
+          En savoir plus
+        </button>
+      </a>
+    </div>
+    <div class="conceptions__articles">
+      <div class="conceptions__articles--content">
+        <p>Les acteurs de l’insertion et du maintien dans l’emploi vous accompagnent
+financièrement</p>
+      </div>
+      <a href="#">
+        <button class="mdc-button mdc-button--raised mdc-ripple-upgraded">
+          En savoir plus
+        </button>
+      </a>
+    </div>
+    <div class="conceptions__articles">
+      <div class="conceptions__articles--content">
+        <p>Pinson Paysage, cette entreprise qui a réussit grâce à l’embauche de travailleurs handicapés </p>
+      </div>
+      <a href="#">
+        <button class="mdc-button mdc-button--raised mdc-ripple-upgraded">
+          En savoir plus
+        </button>
+      </a>
+    </div>
+  </div>
+</div>
